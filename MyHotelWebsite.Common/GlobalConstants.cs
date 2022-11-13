2a35f84f327c6a30918711b41781019f9eafa890
GalinaBujurdzhieva <gal.vins301184@gmail.com>
2022-11-13 16:41:16 +0200

    Working on Home Page, Layout and Room Index View

--- MyHotelWebsite.Common/GlobalConstants.cs
+++ MyHotelWebsite.Common/GlobalConstants.cs
@@ -4,6 +4,16 @@
     {
         public const string SystemName = "MyHotelWebsite";
 
-        public const string AdministratorRoleName = "Administrator";
+        public const string HotelAdministratorRoleName = "Manager"; // ACCESS TO
+
+        public const string WebsiteAdministratorRoleName = "WebAdministrator";  // ACCESS TO BLOGCONTROLLER & GUESTCOMMENTSCONTROLLER ??
+
+        public const string ReceptionistRoleName = "Receptionist"; // ACCESS TO RESERVATIONSCONTROLLER
+
+        public const string ChefRoleName = "Chef";  // ACCESS TO DISHESCONTROLLER
+
+        public const string MaidRoleName = "Maid";  // ACCESS TO ROOMSCONTROLLER
+
+        public const string WaiterRoleName = "Waiter"; // ACCESS TO ORDERSCONTROLLER
     }
 }
