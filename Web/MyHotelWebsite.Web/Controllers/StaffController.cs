a00ea4d13eb865c33d30aeb39b0a7570d6a20ed8
GalinaBujurdzhieva <gal.vins301184@gmail.com>
2022-11-14 17:34:56 +0200

    Working on Gallery View

---
+++ Web/MyHotelWebsite.Web/Controllers/StaffController.cs
@@ -0,0 +1,14 @@
+﻿namespace MyHotelWebsite.Web.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize]
+    public class StaffController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
