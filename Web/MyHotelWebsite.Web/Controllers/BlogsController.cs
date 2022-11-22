e7f6e87c0f8a678e8c0b5b359086ccdc9fd3a017
GalinaBujurdzhieva <gal.vins301184@gmail.com>
2022-11-22 10:12:42 +0200

    Working on BlogAll and NewestBlogs View

--- Web/MyHotelWebsite.Web/Controllers/BlogsController.cs
+++ Web/MyHotelWebsite.Web/Controllers/BlogsController.cs
@@ -1,27 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using MyHotelWebsite.Services.Data;
-using MyHotelWebsite.Web.ViewModels.Blogs;
-
-namespace MyHotelWebsite.Web.Controllers
+﻿namespace MyHotelWebsite.Web.Controllers
 {
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+    using MyHotelWebsite.Services.Data;
+    using MyHotelWebsite.Web.ViewModels.Blogs;
+    using System.IO;
+
     public class BlogsController : BaseController
     {
         private readonly IBlogsService blogService;
-        public BlogsController(IBlogsService blogService)
+
+        public BlogsController(IBlogsService blogService, IWebHostEnvironment hostingEnvironment)
         {
             this.blogService = blogService;
         }
 
         public IActionResult Index()
         {
-            return this.View();
+            var model = new BlogAllViewModel
+            {
+                Blogs = this.blogService.GetLastBlogs<SingleBlogViewModel>(2),
+            };
+            return this.View(model);
         }
 
-        public IActionResult All(int id)
+        public IActionResult All(int id = 1)
         {
             var model = new BlogAllViewModel
             {
-                Blogs = this.blogService.GetAllBlogs(id, 4),
+                Blogs = this.blogService.GetAllBlogs<SingleBlogViewModel>(id, 4),
                 PageNumber = id,
             };
             return this.View(model);
