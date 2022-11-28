2266f7e5ee938022d4c9e981da0532249f44736d
GalinaBujurdzhieva <gal.vins301184@gmail.com>
2022-11-28 14:15:52 +0200

    Found appropriate Admin NavBar

--- Web/MyHotelWebsite.Web/Areas/Administration/Controllers/BlogsController.cs
+++ Web/MyHotelWebsite.Web/Areas/Administration/Controllers/BlogsController.cs
@@ -1,17 +1,15 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using MyHotelWebsite.Data.Models;
-using MyHotelWebsite.Services.Data;
-using MyHotelWebsite.Web.ViewModels.Administration.Blogs;
-using System;
-using System.Linq;
-using System.Security.Claims;
-using System.Security.Policy;
-using System.Threading.Tasks;
-
-namespace MyHotelWebsite.Web.Areas.Administration.Controllers
+﻿namespace MyHotelWebsite.Web.Areas.Administration.Controllers
 {
+    using System;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+    using MyHotelWebsite.Services.Data;
+    using MyHotelWebsite.Web.ViewModels.Administration.Blogs;
+    using MyHotelWebsite.Web.ViewModels.Blogs;
+
     public class BlogsController : AdministrationController
     {
         private readonly IBlogsService blogService;
@@ -52,11 +50,29 @@ namespace MyHotelWebsite.Web.Areas.Administration.Controllers
             return this.RedirectToAction("All", "Blogs", new { area = " " });
         }
 
+        public async Task<IActionResult> All(int id = 1)
+        {
+            if (id < 1)
+            {
+                return this.BadRequest();
+            }
+
+            const int BlogsPerPage = 6;
+            var model = new BlogAllViewModel
+            {
+                ItemsPerPage = BlogsPerPage,
+                AllEntitiesCount = await this.blogService.GetCountAsync(),
+                Blogs = await this.blogService.GetAllBlogsAsync<SingleBlogViewModel>(id, BlogsPerPage),
+                PageNumber = id,
+            };
+            return this.View(model);
+        }
+
         [HttpPost]
         public async Task<IActionResult> Delete(int id)
         {
             await this.blogService.DeleteBlogAsync(id);
-            return this.RedirectToAction("All", "Blogs", new { area = " " });
+            return this.RedirectToAction("All", "Blogs", new { area = "Administration" });
         }
     }
 }
