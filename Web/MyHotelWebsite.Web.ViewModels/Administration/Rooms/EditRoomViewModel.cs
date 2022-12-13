778ce478f0b1af8275313415596a48dfd2026706
GalinaBujurdzhieva <gal.vins301184@gmail.com>
2022-12-13 23:54:55 +0200

    Working on Room Edit

---
+++ Web/MyHotelWebsite.Web.ViewModels/Administration/Rooms/EditRoomViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace MyHotelWebsite.Web.ViewModels.Administration.Rooms
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    using MyHotelWebsite.Data.Models;
+    using MyHotelWebsite.Services.Mapping;
+
+    public class EditRoomViewModel : IMapFrom<Room>
+    {
+        public int Id { get; set; }
+
+        [Range(typeof(decimal), "0.01", "79228162514264337593543950335", ConvertValueInInvariantCulture = true)]
+        public decimal AdultPrice { get; set; }
+
+        [Range(typeof(decimal), "0.01", "79228162514264337593543950335", ConvertValueInInvariantCulture = true)]
+        public decimal ChildrenPrice { get; set; }
+
+        public bool IsReserved { get; set; }
+
+        public bool IsOccupied { get; set; }
+
+        public bool IsCleaned { get; set; }
+    }
+}
