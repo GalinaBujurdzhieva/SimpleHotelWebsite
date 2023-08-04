43de5e205c6132f58072a3fe3ee60ad137f6254c
GalinaBujurdzhieva <gal.vins301184@gmail.com>
2023-08-04 11:22:49 +0300

    Working on Admin Register New Employee

---
+++ Web/MyHotelWebsite.Web.ViewModels/Administration/Staff/RegisterStaffViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MyHotelWebsite.Web.ViewModels.Administration.Staff
+{
+    using System.Collections.Generic;
+
+    //using Microsoft.AspNetCore.Mvc.Rendering;
+    using MyHotelWebsite.Web.ViewModels.User;
+
+    public class RegisterStaffViewModel : RegisterViewModel
+    {
+        public string Role { get; set; }
+
+        //public IEnumerable<SelectListItem> RoleList { get; set; }
+    }
+}
---
+++ Web/MyHotelWebsite.Web/Areas/Administration/Controllers/StaffController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyHotelWebsite.Data.Models;
+using MyHotelWebsite.Web.ViewModels.User;
+using System.Threading.Tasks;
+
+namespace MyHotelWebsite.Web.Areas.Administration.Controllers
+{
+    public class StaffController : AdministrationController
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<ApplicationRole> roleManager;
+
+        public StaffController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager)
+        {
+                this.signInManager = signInManager;
+                this.userManager = userManager;
+                this.roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            RegisterViewModel model = new RegisterViewModel();
+            return this.View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            var user = new ApplicationUser
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                PhoneNumber = model.PhoneNumber,
+            };
+
+            var result = await this.userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                var guestRole = await this.roleManager.FindByNameAsync("Guest");
+                if (guestRole != null)
+                {
+                    IdentityResult roleResult = await this.userManager.AddToRoleAsync(user, "Guest");
+                }
+
+                await this.signInManager.SignInAsync(user, isPersistent: false);
+                return this.RedirectToAction("Login", "User");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                this.ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            return this.View(model);
+        }
+    }
+}
